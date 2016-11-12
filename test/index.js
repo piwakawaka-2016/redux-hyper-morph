966b8f0847c600b19eee46e052af8d0a1a5c5048
Piet Geursen <pietgeursen@gmail.com>
2016-11-13 00:37:14 +1300

    Add test.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,20 @@
 var test = require('tape')
+var freeze = require('deep-freeze')
 
-test('testing redux-hyper-morph', function (t) {
-  t.ok(true)
+var reducer = require('../reducer');
+
+test('ADD_TO_CART adds one when there is none', function(t) {
+  var initialState = {
+    cart: {
+     
+    } 
+  }
+  var newState = reducer(initialState, {type: 'ADD_TO_CART', payload: 1})
+  t.equal(newState.cart[1], 1)
+  t.end()
+})
+
+test('REMOVE_FROM_CART', function(t) {
+
   t.end()
 })
