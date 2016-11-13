2427ee01e90103ec264b7a1c5e1bd9d610dd0a6b
Piet Geursen <pietgeursen@gmail.com>
2016-11-13 17:01:35 +1300

    Add and pass tests for removing from cart.

--- reducer.js
+++ reducer.js
@@ -5,6 +5,17 @@ module.exports = function reducer (state, action) {
   switch (action.type) {
     case 'INIT':
       return newState
+    case 'REMOVE_FROM_CART':
+      var cart = newState.cart
+      var productId = action.payload
+
+      cart[productId] = cart[productId] - 1  
+      if (!cart[productId]) {
+        delete cart[productId] 
+      }
+
+      return newState
+      
     case 'ADD_TO_CART':
       var cart = newState.cart
       var productId = action.payload
--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var freeze = require('deep-freeze')
 
 var reducer = require('../reducer');
 
-test('ADD_TO_CART adds one when there is none', function(t) {
+test('ADD_TO_CART adds one when that item is not in the cart', function(t) {
   var initialState = {
     cart: {
      
@@ -14,7 +14,7 @@ test('ADD_TO_CART adds one when there is none', function(t) {
   t.end()
 })
 
-test('ADD_TO_CART adds one when there are already some', function(t) {
+test('ADD_TO_CART adds one when there are already some of that item in the cart', function(t) {
   var initialState = {
     cart: {
       '1': 2
@@ -25,7 +25,24 @@ test('ADD_TO_CART adds one when there are already some', function(t) {
   t.end()
 })
 
-test('REMOVE_FROM_CART', function(t) {
+test('REMOVE_FROM_CART when there are not any in the cart', function(t) {
 
+  var initialState = {
+    cart: {
+    } 
+  }
+  var newState = reducer(initialState, {type: 'REMOVE_FROM_CART', payload: 1})
+  t.deepEqual(newState, {cart: {}})
+  t.end()
+})
+
+test('REMOVE_FROM_CART when there are some in the cart', function(t) {
+  var initialState = {
+    cart: {
+      '1': 2
+    } 
+  }
+  var newState = reducer(initialState, {type: 'REMOVE_FROM_CART', payload: 1})
+  t.deepEqual(newState, {cart: {'1': 1}})
   t.end()
 })
