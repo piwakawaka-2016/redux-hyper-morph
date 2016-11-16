abb81280344770c8dd061f67ddf2d5b9fbed3b18
Piet Geursen <pietgeursen@gmail.com>
2016-11-16 16:45:39 +1300

    Updates

---
+++ index.js
@@ -0,0 +1,39 @@
+var redux = require('redux')
+var h = require('hyperscript')
+var morphdom = require('morphdom')
+
+var reducer = require('./reducer')
+var productsTemplate = require('./views/products')
+var cartTemplate = require('./views/cart')
+
+var main = document.querySelector('main')
+var app = document.createElement('div')
+main.appendChild(app)
+
+var initialState = {
+  products: [
+    {id: 1, name: 'The Name of the Wind', price: 12.50, details: 'Kvothe is cool'},
+    {id: 2, name: 'Firefall', price: 11.29, details: 'Aliens are scary. Space vampires'},
+    {id: 3, name: 'Kingdom of fear', price: 34.33, details: 'Song of the sausage creature'}
+  ],
+  cart: {
+    1: 2 // There are two items with id 1 in the cart
+  }
+}
+
+var store = redux.createStore(reducer, initialState)
+
+store.subscribe(function () {
+  var state = store.getState()
+  var view = render(state, store.dispatch)
+  morphdom(app, view)
+})
+
+store.dispatch({type: 'INIT'})
+
+function render (state, dispatch) {
+  return h('div#app', {}, [
+    productsTemplate(state.products, dispatch),
+    cartTemplate(state, dispatch)
+  ])
+}
---
+++ reducer.js
@@ -0,0 +1,31 @@
+var clone = require('clone')
+
+module.exports = function reducer (state, action) {
+  var newState = clone(state)
+  switch (action.type) {
+    case 'INIT':
+      return newState
+    case 'REMOVE_FROM_CART':
+      var cart = newState.cart
+      var productId = action.payload
+
+      cart[productId] = cart[productId] - 1
+      if (!cart[productId]) {
+        delete cart[productId]
+      }
+      return newState
+
+    case 'ADD_TO_CART':
+      var cart = newState.cart
+      var productId = action.payload
+
+      cart[productId] = cart[productId] + 1 || 1
+      return newState
+    case 'TOGGLE_DETAILS':
+      var product = newState.products.find(product => product.id === action.payload) 
+      product.showDetails = !product.showDetails
+      return newState
+  }
+  return newState
+}
+
