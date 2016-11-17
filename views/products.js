a50968d15119de921ee6d95436bf8bd61744e714
kfrn <knfrances@gmail.com>
2016-11-17 17:27:51 +1300

    Render price on page

--- views/products.js
+++ views/products.js
@@ -2,6 +2,10 @@ var h = require('hyperscript')
 
 module.exports = ({products}, dispatch) => {
   return h('div#products', {}, products.map((product, index) => {
-    return h('p', {className: 'name'}, product.name)
+    return [
+      h('p', {className: 'name'}, product.name),
+      h('p', "Price: ", product.price)
+    ]
+
   }))
 }
